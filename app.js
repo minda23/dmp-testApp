83e97594c738821430a25a2bcc240b40d1374e7e
minda23 <luki.mindas@gmail.com>
2025-12-22 16:26:36 +0100

    Add new name for unordered list

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ let setNewColor = function () {
   const Carcolor = document.getElementById("Carcolor");
   Carcolor.innerHTML = "";
 };
+
+let counterBankAccount = 30;
+let counter = 20;
+
+let result = counterBankAccount + counter;
+ssdwqd;
